78d39f90df86149a79f9f96e049f5cef8a008f70
Meesch <meesstip@gmail.com>
2023-03-09 17:15:13 +0100

    add xml output

--- igtdetect/pdf2gloss.py
+++ igtdetect/pdf2gloss.py
@@ -4,6 +4,7 @@ import subprocess
 import glossharvester
 import logging
 from pathlib import Path
+import xml.etree.ElementTree as ET
 
 
 def main(input_path, output_path, model_path='../sample/new-model.pkl.gz', config_path='../defaults.ini.sample'):
@@ -27,7 +28,7 @@ def main(input_path, output_path, model_path='../sample/new-model.pkl.gz', confi
 
     IGT_list = harvest_glosses(detected_igts)
 
-    save_glosses_as_txt(IGT_list, output_path)
+    save_glosses_as_xml(IGT_list, output_path)
 
 
 def setup_temp_dir(output_path):
@@ -48,18 +49,23 @@ def scan_pdfs(input_path, temp_path):
     Iterates over a directory to find PDFs and converts them to txt files.
     Returns path to the txt file directory.
     '''
+    scanned_count = 0
     scanned_files_path = temp_path / 'txt'
     for filename in os.listdir(input_path):
         if filename.lower().endswith('.pdf'):
             path_to_pdf = input_path / filename
-            text_file = os.path.basename(path_to_pdf).split('.pdf')[0] + '-scanned.txt'
+            text_file = os.path.splitext(path_to_pdf)[0] + '-scanned.txt'
             path_to_txt = scanned_files_path / text_file
-            subprocess.run(['pdf2txt.py', '-t', 'xml', '-o', path_to_txt, path_to_pdf])
-            logging.info("Scanned {}".format(filename))
+            try:
+                subprocess.run(['pdf2txt.py', '-t', 'xml', '-o', path_to_txt, path_to_pdf])
+                logging.info("Scanned {}".format(filename))
+                scanned_count += 1
+            except:
+                logging.error('PDF scan failed for: {}'.format(filename))
         else:
             logging.info("Could not process: {} - Not a PDF.".format(filename))
     
-    logging.info("PDF scanning complete, scanned {} files".format(len(os.listdir(scanned_files_path))))
+    logging.info("PDF scanning complete, scanned {} files".format(scanned_count))
     return scanned_files_path
 
 
@@ -68,18 +74,23 @@ def get_features_from_txts(input_path, temp_path):
     Iterates over txt files in a directory in order to derive the features from them.
     Returns a path to a directory with freki files.
     '''
+    features_count = 0
     features_path = temp_path / 'features'
     for filename in os.listdir(input_path):
         if filename.endswith('.txt'):
             path_to_txt = input_path / filename
             filename = os.path.basename(filename).split('-scanned')[0] + '-features.txt'
             path_to_feature = features_path / filename
-            subprocess.run(['freki', path_to_txt, path_to_feature, '-r', 'pdfminer'])
-            logging.info("Got features from " + filename)
+            try:
+                subprocess.run(['freki', path_to_txt, path_to_feature, '-r', 'pdfminer'])
+                logging.info("Got features from {}".format(filename))
+                features_count += 1
+            except:
+                logging.error('Freki analysis failed for: {}'.format(filename))
         else:
             logging.info("Could not get features from: {} - Not a txt.".format(filename))
     
-    logging.info("Feature analysis complete: {} files analyzed.".format(len(os.listdir(features_path))))
+    logging.info("Feature analysis complete: {} files analyzed.".format(features_count))
     return features_path
 
 
@@ -89,8 +100,11 @@ def detect_igts(input_path, temp_path, model_path, config_path):
     Runs the igt-detect script with a provided model or config, resulting in a freki features file with tags
     '''
     analyzed_features_path = temp_path / 'analyzed_features'
-    subprocess.run(['python', './detect-igt', 'test', '--config', config_path, '--classifier-path', model_path, '--test-files', input_path, '--classified-dir', analyzed_features_path])
-    logging.info('igt-detect finished: analyzed {} files'.format(len(os.listdir(analyzed_features_path))))
+    try:
+        subprocess.run(['python', './detect-igt', 'test', '--config', config_path, '--classifier-path', model_path, '--test-files', input_path, '--classified-dir', analyzed_features_path])
+        logging.info('igt-detect finished: analyzed {} files'.format(len(os.listdir(analyzed_features_path))))
+    except:
+        logging.error('igt-detect failed')
     return analyzed_features_path
 
 
@@ -117,10 +131,29 @@ def save_glosses_as_txt(IGT_list, output_path):
         for item in IGT_list:
             file.writelines(str(item) + "\n")
 
+def save_glosses_as_xml(IGT_list, output_path):
+    glosses = ET.Element('Glosses')
+    for index, item in enumerate(IGT_list):
+        gloss = ET.SubElement(glosses, 'gloss_'+str(index))
+        meta = ET.SubElement(gloss, 'metadata')
+        meta.set('source', item.source)
+        meta.set('linenr', str(item.linenr))
+        meta.set('classicifaction_methods', item.classification_methods)
+        content = ET.SubElement(gloss, 'content')
+        content.set('line', item.line)
+        content.set('gloss', item.gloss)
+        content.set('translation', item.translation)
+
+    filename = "IGTs_harvested.xml"
+    glosses_tree = ET.ElementTree(glosses)
+    ET.indent(glosses_tree)
+    
+    with open(os.path.join(output_path, filename), 'w') as file:
+        glosses_tree.write(file, encoding='unicode')
 
 if __name__ == '__main__':
     input_path = sys.argv[1]
     output_path = sys.argv[2]
     model_path = sys.argv[3]
     config_path = sys.argv[4]
-    main(input_path, output_path, model_path, config_path) 
+    main(input_path, output_path, model_path, config_path)
